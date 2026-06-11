52f5ed0b856cb04efee56aadb48f1f28fe987c37
monykybarbosa-glitch <monyky.barbosa@escola.pr.gov.br>
2026-06-11 15:27:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+function saibaMais() {
+    document
+    .getElementById("sobre")
+    .scrollIntoView({
+        behavior: "smooth"
+    });
+}
 
+document
+.getElementById("formContato")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    alert("Mensagem enviada com sucesso!");
+
+    this.reset();
+});
